3764021ec94a6fb59d9127202863104b2ad6bd01
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-21 20:31:21 +0200

    Refactor getProduct method to use @PathVariable annotation for improved URL mapping

--- src/main/java/com/walter/ecom_proj/controller/ProductController.java
+++ src/main/java/com/walter/ecom_proj/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.walter.ecom_proj.controller;
 import com.walter.ecom_proj.model.Product;
 import com.walter.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,7 +28,7 @@ public class ProductController {
     }
 
     @GetMapping("/product/{Id}")
-    public Product getProduct(int Id){
+    public Product getProduct(@PathVariable int Id){
         return service.getProductById(Id);
     }
 }
